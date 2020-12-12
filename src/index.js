3c56a914fdc3150a3ee400c58f9b030837f2e65a
Viktor Tsolka <bobick.zdoh@gmail.com>
2020-12-12 14:23:58 +0200

    заканчивает дз

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './styles.css';
+
+import colors from './js/colors';
+import { randomIntegerFromInterval } from './js/function-random';
+
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-action="start"]');
+const btnStop = document.querySelector('button[data-action="stop"]');
+
+btnStart.addEventListener('click', onbtnStartClick);
+btnStop.addEventListener('click', onbtnStop);
+
+let timerId = null;
+
+function onbtnStartClick(event) {
+  event.target.disabled = true;
+  event.target.classList.add('disabled');
+  timerId = setInterval(randomColor, 1000);
+}
+
+function onbtnStop() {
+  //   body.style.backgroundColor = '';
+  btnStart.disabled = false;
+  btnStart.classList.remove('disabled');
+  clearInterval(timerId);
+}
+
+function randomColor() {
+  const color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  body.style.backgroundColor = color;
+}
